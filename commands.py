f170a973dfb2d9b0d9af51f40321ff959f8623c3
Demyan Seleznev <seleznev@intervolga.ru>
2019-03-29 17:30:48 +0400

    dev: attempted to create CommandEntity

--- commands.py
+++ commands.py
@@ -1,15 +1,68 @@
-from tools import Singleton
+from entity import *
+import sys
 
-@Singleton
-class CommandManager:
-    def __init__(self):
-        self.__commands = ['!начать', '!описание', '!очки', '!выйти']
+class CommandEntity(Entity):
+	def initialize(self):
+		self.arguments = {
+			'help': False,
+			'verbose': False,
+			'project': '',
+			'module': ''
+		}
+		self.rawArguments = sys.argv
+		self.isPrepared = False
 
-    def processCommand(self, string):
-        signal = self.__commands.find(string)
-        if signal < 0:
-            return False
-        self.__lastSignal = signal
+	def prepareArguments(self):
+		scriptname = self.rawArguments.pop(0)
+		self.getSimpleCommands()
+		self.getComplexCommands()
+		self.isPrepared = True
 
-    def getSignal(self):
-        return self.__lastSignal
+	def getCommandAliases(self, aliasName):
+		aliases = {
+            'project': ['-p', '--project'],
+            'module': ['-n', '--name'],
+            'help': ['-h', '--help', '?'],
+            'verbose': ['-v', '--verbose']
+        }
+		if aliasName not in aliases:
+			return []
+		return aliases[aliasName]
+
+	def getSimpleCommands(self):
+		verbose = self.getCommandAliases('verbose')
+		help = self.getCommandAliases('help')
+		indexes = []
+		for argumentIndex, argumentValue in enumerate(self.rawArguments):
+			if argumentValue in verbose:
+				self.arguments['verbose'] = True
+				indexes.append(argumentIndex)
+			if argumentValue in help:
+				self.arguments['help'] = True
+				indexes.append(argumentIndex)
+		indexes.reverse()
+		for index in indexes:
+			del self.rawArguments[index]
+
+	def getComplexCommands(self):
+		project = self.getCommandAliases('project')
+		module = self.getCommandAliases('module')
+		indexes = []
+		for argumentIndex, argumentValue in enumerate(self.rawArguments):
+			try:
+				if argumentValue in project:
+					self.arguments['project'] = self.rawArguments[argumentIndex+1]
+				if argumentValue in module:
+					self.arguments['module'] = self.rawArguments[argumentIndex+1]
+			except IndexError:
+				continue
+
+	def getArgs(self):
+		if self.isPrepared == False:
+			self.prepareArguments()
+		return self.arguments
+
+if __name__ == '__main__':
+	commandObj = CommandEntity()
+	commandObj.initialize()
+	print(commandObj.getArgs())
